628393d583e86f7b8730190d019aee423e7e93cd
Gagan Bansal <gaganbansal123@gmail.com>
2015-07-23 10:25:46 -0400

    input passed as single object, ID2

--- emptymap.js
+++ emptymap.js
@@ -10,22 +10,23 @@ var em = function(viewportSize,options) {
     bottom: -20037508.342789244,
     top: 20037508.342789244
   };
-  this.tSize = this.opt.tileSize || 256
+  this.tSize = this.opt.tileSize || 256;
   this.maxRes = Math.min(
     Math.abs(this.projExt.right - this.projExt.left)/this.tSize,
     Math.abs(this.projExt.top - this.projExt.bottom)/this.tSize);
-  this.curScale = 1;
-  this.curRotation = 0;
   this.matrix = new Matrix();
   this.tileMatrix = new Matrix();
   if(this.opt.view && this.opt.callback)
     this.setVeiw(this.opt.view,this.opt.callback,this.opt.scope);
 };
 
-em.prototype.setView = function(view, cb, scope) {
+//em.prototype.setView = function(view, cb, scope) {
+em.prototype.setView = function(params) {
+  var view = params.view, 
+    cb = params.callback,
+    scope = params.scope || this;
   if (typeof(cb) !== 'function') {
-    cb.call(null,'callback is not function',null);
-    return;
+    throw new TypeError('cb is not function');
   }
   if(view ) {
     view.center = view.center || [0,0];
@@ -33,61 +34,51 @@ em.prototype.setView = function(view, cb, scope) {
       ? Math.pow(2,view.zoom) : 1;
     view.rotation = view.rotation || 0;
   } else {
-    cb.call(null,'view details are wrong',null);
+    cb.call(scope,new Error('view details are wrong'));
     return;
   }
   this._setAt(view.center, view.scale, view.rotation);
-  var scope = scope || null;
-  cb.call(scope,null,this.matrix.m,this);
+  cb.call(scope, null,{
+    matrix: this.matrix.m,
+    tileMatrix: this.tileMatrix.m,
+    map:this});
 };
 
-em.prototype._setAt = function(ctr, scale, rot) {
-  var basePx = this._latLongToBasePx(ctr);
-  this.matrix.reset();
-  this.matrix.translate(basePx[0],basePx[1])
-    .rotate(Math.PI/180*rot)
-    .scale(scale,scale)
-    .translate(-basePx[0],-basePx[1]);
-  var baseCenter = this.matrix.inverse()
-    .transformPoint(this.vpSize.width/2,this.vpSize.height/2);
-  this.matrix.translate(baseCenter[0] - basePx[0], baseCenter[1] - basePx[1]);
-  // for tile matrix
-  this.tileMatrix.reset();
-  this.tileMatrix.translate(this.vpSize.width/2,this.vpSize.height/2)
-    .rotate(rot * Math.PI/180)
-    .translate(-this.vpSize.width/2,-this.vpSize.height/2)
-};
-
-em.prototype._latLongToBasePx = function(latLong) {
-  return [
-    (latLong[0] - this.projExt.left)/this.maxRes,
-    (this.projExt.top - latLong[1])/this.maxRes];
-};
-em.prototype._basePxToLatLong = function(px) {
-  return [
-    this.projExt.left + px[0]*this.maxRes,
-    this.projExt.top - px[1]*this.maxRes];
-}
-em.prototype.moveByPixel = function(delta,cb,scope) {
-  if (typeof cb !== 'function') {
-    cb.call(null,'callback is not function',null);
-    return;
+em.prototype.applyDeltaMove = function(params) {
+  var deltaX = params.deltaX || 0,
+    deltaY = params.deltaY || 0,
+    cb = params.callback,
+    scope = params.scope || this;
+  if (typeof(cb) !== 'function') {
+    throw new TypeError('cb is not function');
   }
-  var basePx = this.matrix.inverse().transformPoint(delta[0],delta[1]);
+  var basePx = this.matrix.inverse().transformPoint(deltaX,deltaY);
   var baseVPTopLeftPx = this.matrix.inverse().transformPoint(0,0);
-  this.matrix.translate(basePx[0] - baseVPTopLeftPx[0], basePx[1] - baseVPTopLeftPx[1]);
+  this.matrix.translate(
+    basePx[0] - baseVPTopLeftPx[0], basePx[1] - baseVPTopLeftPx[1]);
   // apply for tile matrix
-  basePx = this.tileMatrix.inverse().transformPoint(delta[0],delta[1]);
+  basePx = this.tileMatrix.inverse().transformPoint(deltaX,deltaY);
   baseVPTopLeftPx = this.tileMatrix.inverse().transformPoint(0,0);
-  this.tileMatrix.translate(basePx[0] - baseVPTopLeftPx[0], basePx[1] - baseVPTopLeftPx[1]);
-  
-  cb.call(scope,null,this.matrix.m,this);
-}
-em.prototype.applyDeltaScaleRotation = function(vpPx,factor, rot, cb, scope) {
-  var factor = factor || 1,
-    rot = rot || 0,
-    basePx = this.matrix.inverse().transformPoint(vpPx[0],vpPx[1]),
-    destBasePx;
+  this.tileMatrix.translate(
+    basePx[0] - baseVPTopLeftPx[0], basePx[1] - baseVPTopLeftPx[1]);
+  cb.call(scope, null,{
+    matrix: this.matrix.m,
+    tileMatrix: this.tileMatrix.m,
+    map:this});
+};
+//em.prototype.applyDeltaScaleRotation = function(vpPx,factor, rot, cb, scope) {
+em.prototype.applyDeltaScaleRotation = function(params) {
+  //TODO vpPx to be renamed
+  var vpPx = params.position, 
+    factor = params.factor || 1,
+    rot = params.rotation || 0,
+    cb = params.callback,
+    scope = params.scope || this,
+    basePx, destBasePx;
+  if (typeof(cb) !== 'function') {
+    throw new TypeError('cb is not function');
+  }
+  basePx = this.matrix.inverse().transformPoint(vpPx[0],vpPx[1]),
   this.matrix.translate(basePx[0],basePx[1])
     .rotate(Math.PI*rot/180)
     .scale(factor,factor)
@@ -98,39 +89,41 @@ em.prototype.applyDeltaScaleRotation = function(vpPx,factor, rot, cb, scope) {
     .rotate(Math.PI*rot/180)
     .scale(factor,factor)
     .translate(-basePx[0],-basePx[1])
-  cb.call(scope,null,this.matrix.m,this);
-}
+  cb.call(scope, null,{
+    matrix: this.matrix.m,
+    tileMatrix: this.tileMatrix.m,
+    map:this});
+};
+
+em.prototype.resetTileMatrix = function(params) {
+  var cb = params.callback,
+    scope = params.scope || this;
+  if (typeof(cb) !== 'function') {
+    throw new TypeError('cb is not function');
+  }
+  var rot = Math.atan2(this.tileMatrix.m[1],this.tileMatrix.m[0]);
+  this.tileMatrix.reset();
+  this.tileMatrix.translate(this.vpSize.width/2,this.vpSize.height/2)
+    .rotate(rot)
+    .translate(-this.vpSize.width/2,-this.vpSize.height/2);
+  cb.call(scope, null,{
+    matrix: this.matrix.m,
+    tileMatrix: this.tileMatrix.m,
+    map:this});
+};
  
-em.prototype.vpPxToLatLong = function(px) {
+em.prototype.vieportToLongLat = function(px) {
   var basePx = this.matrix.inverse().transformPoint(px[0],px[1]);
-  return this._basePxToLatLong(basePx);
+  return this._basePxToLongLat(basePx);
 };
 
-em.prototype.latLongToVPPx = function(latLong) {
-  var basePx = this._latLongToBasePx(latLong);
+em.prototype.longLatToViewport = function(longLat) {
+  var basePx = this._longLatToBasePx(longLat);
   return this.matrix.transformPoint(basePx[0],basePx[1]);
-};
-
-em.prototype.zoomIn = function(cb, scope) {
-  this.applyDeltaScaleRotation(
-    [this.vpSize.width/2, this.vpSize.height/2],
-    2,0, cb, scope
-  );
-};
-
-em.prototype.zoomOut = function(cb, scope) {
-  this.applyDeltaScaleRotation(
-    [this.vpSize.width/2, this.vpSize.height/2],
-    .5, 0, cb, scope 
-  );
 };
 
 em.prototype.getExtent = function() {
   return {
-    left: this.curTopLeft.long,
-    top: this.curTopLeft.lat,
-    right: this.curTopLeft.long + this.vpSize.width * (this.maxRes / this.curScale),
-    bottom: this.curTopLeft.lat - this.vpSize.height * (this.maxRes / this.curScale)
   };
 }
 em.prototype.getCenter = function() {
@@ -161,13 +154,32 @@ em.prototype.getView = function() {
     rotation: this.getRotation()
   };
 };
-em.prototype.resetTileMatrix = function(cb, scope) {
-  var rot = Math.atan2(this.tileMatrix.m[1],this.tileMatrix.m[0]);
+
+em.prototype._setAt = function(ctr, scale, rot) {
+  var basePx = this._longLatToBasePx(ctr);
+  this.matrix.reset();
+  this.matrix.translate(basePx[0],basePx[1])
+    .rotate(Math.PI/180*rot)
+    .scale(scale,scale)
+    .translate(-basePx[0],-basePx[1]);
+  var baseCenter = this.matrix.inverse()
+    .transformPoint(this.vpSize.width/2,this.vpSize.height/2);
+  this.matrix.translate(baseCenter[0] - basePx[0], baseCenter[1] - basePx[1]);
+  // for tile matrix
   this.tileMatrix.reset();
   this.tileMatrix.translate(this.vpSize.width/2,this.vpSize.height/2)
-    .rotate(rot)
-    .translate(-this.vpSize.width/2,-this.vpSize.height/2);
-  cb.call(scope,null,this.tileMatrix.m,this);
+    .rotate(rot * Math.PI/180)
+    .translate(-this.vpSize.width/2,-this.vpSize.height/2)
 };
+em.prototype._longLatToBasePx = function(longLat) {
+  return [
+    (longLat[0] - this.projExt.left)/this.maxRes,
+    (this.projExt.top - longLat[1])/this.maxRes];
+};
+em.prototype._basePxToLongLat = function(px) {
+  return [
+    this.projExt.left + px[0]*this.maxRes,
+    this.projExt.top - px[1]*this.maxRes];
+}
 
 module.exports = em; 
